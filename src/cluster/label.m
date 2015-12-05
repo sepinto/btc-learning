7112be2187a05be521a654383a93a8e0667f6b0c
Stephen Pinto <stephen.pinto42@gmail.com>
2015-12-05 15:05:54 -0800

    labelling works. Using exponentially spaced prob bins right now. Could also use linear by uncommenting a line

---
+++ src/cluster/label.m
@@ -0,0 +1,30 @@
+function [ labels, varargout ] = label( y, pdf, k, varargin)
+%LABEL Summary of this function goes here
+%   Detailed explanation goes here
+
+    % Validate and parse input arguments
+    p = inputParser;
+    addParameter(p,'diagnostics',0);
+    parse(p,varargin{:});
+    diagnostics = p.Results.diagnostics;
+    
+    granularity = 1e5;
+    domain = linspace(min(y),max(y),granularity);
+    cdf = cumsum(pdf(domain));
+    cdf = (cdf - min(cdf)) / (max(cdf) - min(cdf)); % Normalize
+
+%     probEdges = linspace(0,1,k+1);
+    probEdges = [0 (1 - 1./exp(1:k)) 1];
+    edges = zeros(k+1,1); edges(end) = Inf;
+    for i=2:k
+        edges(i) = domain(find(cdf >= probEdges(i), 1));
+    end
+    labels = discretize(y, edges);
+    
+    if diagnostics
+        varargout{1} = domain;
+        varargout{2} = cdf;
+        varargout{3} = edges;
+    end
+end
+
--- src/run.m
+++ src/run.m
@@ -8,11 +8,13 @@ load 12022015.mat
 
 %% Cluster and Fit Distribution
 % pdf = fitDistribution(y, 4); % pdf is a function handle
-[pdf, idx, phi, pdfs] = fitDistribution(y, 4, 'diagnostics', 1);
+[pdf, idx, phi, pdfs] = fitDistribution(y, 2, 'diagnostics', 1);
 plotCluster(y, pdf, idx, phi, pdfs, [1 2 3]);
 
 %% Define l equal-probability subdomains
 % labels = label(y, pdf, 10);
+[labels, domain, cdf, edges] = label(y,pdf,10,'diagnostics',1);
+plotSubdomains(y, labels, domain, cdf, edges, [4 5]);
 
 %% Train
 %svmModel = svm(x, y);
