69c36e5f30925809e7bcf7b1967419a2c101f222
Stephen Pinto <stephen.pinto42@gmail.com>
2015-12-06 11:17:58 -0800

    renamed python dir and removed data_understanding dir

--- src/data_understanding/plotexp.m
+++
@@ -1,7 +0,0 @@
-[pe, pdf_expmixture] = fitExponential(y');
-
-figure; hold;
-edges = 1:20:5000;
-histogram(y', edges, 'Normalization', 'pdf');
-x = 1:5000;
-plot(x, pdf_expmixture(x, pe(1), pe(2), pe(3)), 'LineWidth', 2);
